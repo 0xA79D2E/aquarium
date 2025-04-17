c5dcd9da66e2ac2b3100495cd90ad9fbdb7e2fe2
0xA79D2E <167731823+0xA79D2E@users.noreply.github.com>
2025-04-16 20:26:42 -0700

    Update Fish.java

--- src/Fish.java
+++ src/Fish.java
@@ -1,2 +1,19 @@
 public class Fish {
+
+
+    private static int nextId;
+    public final int id;
+    public final Species species;
+
+    public Fish(Species species) {
+        this.id = nextId++;
+        this.species = species;
+    }
+
+
+    public void show() {
+        System.out.printf("Fish #%d | Species %s", id, species.name);
+    }
+
+
 }
